3f4da88601294a88e2d78b8b2705fd3a47609314
Tạ Quang Khôi <45689286+TaQuangKhoi@users.noreply.github.com>
2021-08-12 20:46:10 +0700

    Initial commit

---
+++ PhanSo.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FractionCalculator
+{
+    class PhanSo
+    {
+        int TuSo;
+        int MauSo;
+
+        public static int UocChungLonNhat(int a, int b) // Tìm ra ước chung sai rồi
+        {
+            if (a%b == 0)
+            {
+                return b;
+            }
+            else
+            {
+                return UocChungLonNhat(b, a%b);
+            }
+        }
+        public void nhapPhanSo()
+        {
+            Console.Write("Nhập tử số: ");
+            TuSo = int.Parse(Console.ReadLine());
+            Console.Write("Nhập mẫu số: ");
+            MauSo = int.Parse(Console.ReadLine());
+        }
+        
+        public void xuatPhanSo()
+        {
+            // Console.WriteLine("Phân số của bạn là:" );
+            Console.WriteLine(TuSo);
+            if (TuSo > 9 || MauSo > 9)
+            {
+                Console.WriteLine("---");
+            }
+            else
+            {
+                Console.WriteLine("--");
+            }
+            Console.WriteLine(MauSo);
+        }
+        public void xuatPhanSo(int a, int b)
+        {
+            Console.WriteLine(a);
+            if (a > 9 || b > 9)
+            {
+                Console.WriteLine("---");
+            }
+            else
+            {
+                Console.WriteLine("--");
+            }
+            Console.WriteLine(b);
+        }
+        public int LayTuSo()
+        {
+            return TuSo;
+        }
+        public int LayMauSo()
+        {
+            return MauSo;
+        }
+        public void GanTuSo(int a) 
+        {
+            TuSo = a;
+        }
+        public void GanMauSo(int a)
+        {
+            MauSo = a;
+        }
+        public void NghichDao()
+        {
+            int TuSoDao = MauSo;
+            int MauSoDao = TuSo;
+            Console.WriteLine("Phân số nghịch đảo là: ");
+            Console.WriteLine(TuSoDao);
+            Console.WriteLine("--");
+            Console.WriteLine(MauSoDao);
+        }
+        
+        public void RutGon()
+        {
+            int UocChungLaGi;
+            if (TuSo > MauSo)
+            {
+                UocChungLaGi = UocChungLonNhat(TuSo, MauSo);
+            }
+            else
+            {
+                UocChungLaGi = UocChungLonNhat(MauSo, TuSo); 
+            }
+            int TuSoSauRutGon = TuSo / UocChungLaGi;
+
+            int MauSoSauRutGon = MauSo / UocChungLaGi;
+
+            xuatPhanSo(TuSoSauRutGon, MauSoSauRutGon);
+        }
+        public int mMauSoChung(PhanSo ps)
+        {
+            return this.LayMauSo() * ps.LayMauSo();
+        }
+        public void QuyDongMau(PhanSo ps)
+        {
+            int MauSoChung = this.mMauSoChung(ps);
+            
+            PhanSo ps1m = new PhanSo();
+            PhanSo ps2m = new PhanSo();
+            ps2m.GanMauSo(MauSoChung);
+            ps1m.GanMauSo(MauSoChung);
+            ps1m.GanTuSo(this.LayTuSo() * ps.LayMauSo());
+            ps2m.GanTuSo(ps.LayTuSo() * this.LayMauSo());
+            ps1m.xuatPhanSo();
+            ps2m.xuatPhanSo();
+        }
+        
+        public void CongPhanSo(PhanSo ps)
+        {
+            int TuSoTong = this.LayTuSo()*ps.LayMauSo() + ps.LayTuSo()*this.LayMauSo();
+            int MauSoTong = this.mMauSoChung(ps);
+            xuatPhanSo(TuSoTong, MauSoTong);
+        }
+        public void TruPhanSo(PhanSo ps)
+        {
+            int TuSoHieu = this.LayTuSo() * ps.LayMauSo() - ps.LayTuSo() * this.LayMauSo();
+            int MauSoHieu = this.mMauSoChung(ps);
+            xuatPhanSo(TuSoHieu, MauSoHieu);
+        }
+        public void NhanPhanSo(PhanSo ps)
+        {
+            int TuSoTich = this.LayTuSo() * ps.LayTuSo();
+            int MauSoTich = this.LayMauSo() * ps.LayMauSo();
+            xuatPhanSo(TuSoTich, MauSoTich);
+        }
+        public void ChiaPhanSo(PhanSo ps)
+        {
+            int TuSoThuong = this.LayTuSo() * ps.LayMauSo();
+            int MauSoThuong = ps.LayTuSo() * this.LayMauSo();
+            xuatPhanSo(TuSoThuong, MauSoThuong);
+        }
+        public void SoSanhPhanSo(PhanSo ps)
+        {
+            if (this.LayTuSo() / this.LayMauSo() > ps.LayTuSo() / ps.LayMauSo())
+            {
+                Console.WriteLine("Phân số thứ nhất lớn hơn Phân số thứ hai");
+            }
+            else
+            {
+                Console.WriteLine("Phân số thứ hai lớn hơn Phân số thừ nhất");
+            }
+        }
+        public void XoaPhanSo(PhanSo ps)
+        {
+            this.GanTuSo(0);
+            this.GanMauSo(0);
+            ps.GanTuSo(0);
+            ps.GanMauSo(0);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FractionCalculator
+{
+    class Program
+    {
+        static bool YesNo() // Lựa chọn Yes hoặc No thông qua việc đưa vào giá trị 1 hoặc khác 1
+        {
+            if (Console.ReadLine() == "1")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void DiTiep()
+        {
+            Console.WriteLine("Bạn có muốn tiếp tục dùng chức năng khác? (Enter để tiếp tục)");
+            // bool TiepTuc = YesNo();
+            
+            if (Console.ReadKey().Key == ConsoleKey.Enter)
+            {
+                Console.Clear();
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            int HienPhanSo = 1;
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+
+            Console.Title = "Phần mềm Tính toán Phân số";
+
+            PhanSo ps1 = new PhanSo();
+            PhanSo ps2 = new PhanSo();
+            int VongLap = 1;
+            while (VongLap < 100)
+            {
+                Console.WriteLine("Máy tình Phân số Đơn Giản ({0})",VongLap);
+                Console.WriteLine("   1. Nhập phân số của bạn"); //DONE
+                Console.WriteLine("   2. Hiện các phân số mà tôi vừa nhập"); //DONE
+                Console.WriteLine("   3. Phân Số nghịch đảo");
+                Console.WriteLine("   4. Rút gọn phân số");
+                Console.WriteLine("   5. Quy đồng mẫu hai phân số");
+                Console.WriteLine("   6. Cộng hai phân số");
+                Console.WriteLine("   7. Trừ hai phân số");
+                Console.WriteLine("   8. Nhân hai phân số");
+                Console.WriteLine("   9. Chia hai phân số");
+                Console.WriteLine("   10. So sanh hai phân số");
+                Console.WriteLine("   11. Xóa tất cả phân số!!!");
+                Console.Write("Chọn chức năng bạn muốn sử dụng..! --> ");
+                int ChucNang = int.Parse(Console.ReadLine());
+                switch(ChucNang){
+                    case 1: //Nhập phân số của bạn - DONE
+                        {
+                            if (VongLap > 1 && ps1.LayMauSo() != 0)
+                            {
+                                Console.WriteLine("Bạn muốn nhập phân số một hay hai?");
+                                Console.WriteLine("   Phân số .1.");
+                                Console.WriteLine("   Phân số .2.");
+                                int ChonPhanSo = int.Parse(Console.ReadLine());
+                                switch (ChonPhanSo)
+                                {
+                                    case 1:
+                                        {
+                                            Console.WriteLine("Nhap phân số 1:");
+                                            ps1.nhapPhanSo();
+                                            break;
+                                        }
+                                    case 2:
+                                        {
+                                            Console.WriteLine("Nhap phân số 2:");
+                                            ps2.nhapPhanSo();
+                                            break;
+                                        }
+                                    default:
+                                        Console.WriteLine("Làm gì có phần số nào khác ngoài thứ hai và thứ nhât trong chương trình này chứ");
+                                        break;
+                                }
+                            }
+                            else
+                            {
+                                ps1.nhapPhanSo();
+                                Console.Write("Bạn có muốn nhập phân số thứ hai không? (Enter để đồng ý) --> ");
+                                if (Console.ReadKey().Key == ConsoleKey.Enter)
+                                {
+                                    ps2.nhapPhanSo();
+                                }
+                                else HienPhanSo = 0;
+                            }
+                            Console.Clear();
+                            break;
+                        }
+                    case 2: //Hiện các phân số mà tôi vừa nhập - DONE
+                        {
+                            Console.WriteLine("Phân số thứ nhất là");
+                            ps1.xuatPhanSo();
+                            Console.WriteLine("------------");
+                            if (HienPhanSo == 0)
+                            {
+                                Console.WriteLine("Bạn chưa nhập phân số thứ hai..!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Phân số thứ hai là");
+                                ps2.xuatPhanSo();
+                            }
+                            DiTiep();
+                            break;
+                        }
+                    case 3: // Phân Số nghịch đảo - DONE
+                        {
+                            ps1.NghichDao();
+                            ps2.NghichDao();
+                            break;
+                        }
+                    case 4: // Rút gọn phân số - DONE
+                        { 
+                            ps1.RutGon();
+                            if (ps2.LayMauSo() != 0)
+                            {
+                                ps2.RutGon();
+                            }
+                            else
+                            {
+                                Console.WriteLine("Chưa có phân số hai..!");
+                            }
+                            break;
+                        }
+                    case 5: // Quy đồng mẩu hai phân số - DONE
+                        {
+                            ps1.QuyDongMau(ps2);
+                            break;
+                        }
+                    case 6: // Cộng hai phân số - DONE
+                        {
+                            ps1.CongPhanSo(ps2);
+                            break;
+                        }
+                    case 7: // Trừ hai phân số - DONE
+                        {
+                            ps1.TruPhanSo(ps2);
+                            break;
+                        }
+                    case 8: // Nhân hai phân số - DONE
+                        {
+                            ps1.NhanPhanSo(ps2);
+                            break;
+                        }
+                    case 9: // Chia hai phân số - DONE
+                        {
+                            ps1.ChiaPhanSo(ps2);
+                            break;
+                        }
+                    case 10: // So sanh hai phân số - DONE
+                        {
+                            ps1.SoSanhPhanSo(ps2);
+                            break;
+                        }
+                    case 11: // Xóa tất cả phân số đã nhập - DONE
+                        {
+                            ps1.XoaPhanSo(ps2);
+                            break;
+                        }
+                    default:
+                        Console.Write("Bạn chọn mã chức năng không tồn tại..!");
+                        break;
+                }
+                VongLap++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
